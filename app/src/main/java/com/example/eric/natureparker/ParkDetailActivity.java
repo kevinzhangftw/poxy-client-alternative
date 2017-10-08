fb271cedf6313847beaac124b70df75b9603f3a1
jmloewen <jmloewen@sfu.ca>
2017-10-08 15:23:37 -0700

    cleaned up old commented out code, fixed image loading issue by putting image load into onstart.

--- app/src/main/java/com/example/eric/natureparker/ParkDetailActivity.java
+++ app/src/main/java/com/example/eric/natureparker/ParkDetailActivity.java
@@ -19,23 +19,6 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 public class ParkDetailActivity extends AppCompatActivity {
-
-    /*
-    Mock JSON object for weather
-    * {"coord"=>{"lon"=>-122.92, "lat"=>49.28},
- "weather"=>[{"id"=>802, "main"=>"Clouds", "description"=>"scattered clouds", "icon"=>"03d"}],
- "base"=>"stations",
- "main"=>{"temp"=>20.51, "pressure"=>1015, "humidity"=>64, "temp_min"=>20, "temp_max"=>21},
- "visibility"=>48279,
- "wind"=>{"speed"=>3.1, "deg"=>220},
- "clouds"=>{"a/ll"=>40},
- "dt"=>1502666100,
- "sys"=>{"type"=>1, "id"=>3359, "message"=>0.0041, "country"=>"CA", "sunrise"=>1502629343, "sunset"=>1502681348},
- "id"=>5911606,
- "name"=>"Burnaby",
- "cod"=>200}
-    * */
-    private ImageView imgView;
     private TextView txtName;
     private TextView txtDetails;
     private RelativeLayout mainLayout; //The base layout
@@ -50,6 +33,11 @@ public class ParkDetailActivity extends AppCompatActivity {
         connectFormElements();
 
         parkData = getIntent().getParcelableExtra("Park");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         populateData(parkData);
     }
 
@@ -75,13 +63,8 @@ public class ParkDetailActivity extends AppCompatActivity {
     }
 
     private void populateData(Park thisPark){
-        //TODO: Populate image
-        //imgView.setImageURI(Uri.);
-        //imgView = ......thisPark.getPicture_url()
-        //Picasso.with(getApplicationContext()).load(thisPark.getPicture_url()).fit().into(imgView);
 
         imageString = thisPark.getPicture_url();
-
         if (imageString != null && imageString.length() > 0) {
             Picasso.with(this).load(imageString).into(new Target() {
                 @Override
@@ -99,25 +82,8 @@ public class ParkDetailActivity extends AppCompatActivity {
                     Log.d("oPL", "Bitmap Loading");
                 }
             });
-        }
-        else{
-            Picasso.with(this).load(R.drawable.noparkimage).into(new Target(){
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    mainLayout.setBackground(new BitmapDrawable(bitmap));
-                }
-
-                @Override
-                public void onBitmapFailed(Drawable errorDrawable) {
-                    Log.d("oBF", "Bitmap Failed to load");
-                }
-
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
-                    Log.d("oPL", "Bitmap Loading");
-                }
-            });
-            //Toast.makeText(getApplicationContext(), "No Image", Toast.LENGTH_LONG).show();
+        }else{
+            mainLayout.setBackgroundResource(R.drawable.noparkimage);
         }
 
         txtName.setText(abbrevParkName(thisPark.getName()));
@@ -128,13 +94,7 @@ public class ParkDetailActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), parkName + "", Toast.LENGTH_SHORT).show();
             }
         });
-        /*txtName.setOnHoverListener(new View.OnHoverListener() {
-            @Override
-            public boolean onHover(View view, MotionEvent motionEvent) {
-                Toast.makeText(getApplicationContext(), parkName + "", Toast.LENGTH_SHORT);
-                return true;
-            }
-        });*/
+
         txtDetails.setText("Wind: " + thisPark.getWind() + "\n" +
                            "Temp: " + (int)thisPark.getTemperature() + "°C\n" +
                            "Clouds: " + thisPark.getCloud());
@@ -149,7 +109,5 @@ public class ParkDetailActivity extends AppCompatActivity {
         //parkName = parkName.replace(" Park", "");
         parkName = parkName.replace("Mountain", "Mtn");
         return parkName;
-
     }
-
 }
